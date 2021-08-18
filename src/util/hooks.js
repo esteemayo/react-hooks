1d11a96bc2ebb57bd537db292bea7397307a8e7a
Emmanuel Adebayo <eadebayo15@gmail.com>
2021-08-18 14:27:52 +0100

    first commit

---
+++ src/util/hooks.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export const useForm = (callback, initialState = {}, validate) => {
+    const [values, setValues] = useState(initialState);
+    const [errors, setErrors] = useState({});
+
+    const handleChange = e => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if (Object.keys(validate(values)).length === 0) {
+            callback();
+            setValues(initialState);
+        } else {
+            setErrors(validate(values));
+        }
+    };
+
+    return {
+        handleChange,
+        handleSubmit,
+        errors,
+        values
+    };
+};
