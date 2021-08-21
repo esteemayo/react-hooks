308f673d739cee02b535c3c4cf3875f9feb3a8f7
Emmanuel Adebayo <eadebayo15@gmail.com>
2021-08-21 17:30:59 +0100

    stored people to a localStorage

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,17 +1,29 @@
-import { useContext, useReducer, createContext } from 'react';
+import { useEffect, useContext, useReducer, createContext } from 'react';
 
 import { ADD_PERSON } from './types';
 import items from '../services/data';
 import reducer from './Reducer';
 
+const peopleKey = 'people';
+
+const getLocalStorage = () => {
+    const people = localStorage.getItem(peopleKey);
+    if (people) return JSON.parse(people);
+    return items;
+};
+
 const initialState = {
-    people: items,
+    people: getLocalStorage(),
 };
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        localStorage.setItem(peopleKey, JSON.stringify(state.people));
+    }, [state.people]);
 
     const addPerson = person => {
         dispatch({
