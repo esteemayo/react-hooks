e28d9e78c5a9deb16973a66aa9b9aefece2b349e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-30 20:49:48 +0100

    fix: format code

--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -1,16 +1,16 @@
 import { ADD_PERSON } from './types';
 
 const reducer = (state, action) => {
-    if (action.type === ADD_PERSON) {
-        const newPerson = [...state.people, action.payload];
+	if (action.type === ADD_PERSON) {
+		const newPerson = [...state.people, action.payload];
 
-        return {
-            ...state,
-            people: newPerson,
-        };
-    }
+		return {
+			...state,
+			people: newPerson,
+		};
+	}
 
-    throw new Error('No matching action type');
+	throw new Error('No matching action type');
 };
 
 export default reducer;
