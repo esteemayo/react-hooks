c4dee512760cb06d5f3763a4e69176411f0a77b5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-30 20:50:23 +0100

    fix: format code

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,46 +1,46 @@
 import { useEffect, useContext, useReducer, createContext } from 'react';
 
 import { ADD_PERSON } from './types';
-import items from '../services/data';
 import reducer from './Reducer';
+import items from '../services/data';
 
 const peopleKey = 'people';
 
 const getLocalStorage = () => {
-    const people = localStorage.getItem(peopleKey);
-    if (people) return JSON.parse(people);
-    return items;
+	const people = localStorage.getItem(peopleKey);
+	if (people) return JSON.parse(people);
+	return items;
 };
 
 const initialState = {
-    people: getLocalStorage(),
+	people: getLocalStorage(),
 };
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
+	const [state, dispatch] = useReducer(reducer, initialState);
 
-    useEffect(() => {
-        localStorage.setItem(peopleKey, JSON.stringify(state.people));
-    }, [state.people]);
+	useEffect(() => {
+		localStorage.setItem(peopleKey, JSON.stringify(state.people));
+	}, [state.people]);
 
-    const addPerson = person => {
-        dispatch({
-            type: ADD_PERSON,
-            payload: person
-        });
-    };
+	const addPerson = person => {
+		dispatch({
+			type: ADD_PERSON,
+			payload: person
+		});
+	};
 
-    return (
-        <AppContext.Provider value={{ ...state, addPerson }}>
-            {children}
-        </AppContext.Provider>
-    );
+	return (
+		<AppContext.Provider value={{ ...state, addPerson }}>
+			{children}
+		</AppContext.Provider>
+	);
 };
 
 export const useGlobalContext = () => {
-    return useContext(AppContext);
+	return useContext(AppContext);
 };
 
 export { AppContext, AppProvider };
