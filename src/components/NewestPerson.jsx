21f7100a28b7890763621c58e95f5f7ecd7ab12f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-30 20:49:16 +0100

    fix: format code

--- src/components/NewestPerson.jsx
+++ src/components/NewestPerson.jsx
@@ -1,29 +1,29 @@
 import { useEffect } from 'react';
 
-import { useGlobalContext } from '../context/Context';
 import PeopleCount from './PeopleCount';
+import { useGlobalContext } from '../context/Context';
 
 const NewestPerson = () => {
-    const { people } = useGlobalContext();
-    const newestPerson = people[people.length - 1];
+	const { people } = useGlobalContext();
+	const newestPerson = people[people.length - 1];
 
-    useEffect(() => {
-        const newestPersonName = `${newestPerson.firstName} ${newestPerson.lastName}`;
-        document.title = newestPersonName;
-        console.log('useEffect');
-        return () => {
-            console.log('Unmounted');
-        }
-    }, [newestPerson]);
+	useEffect(() => {
+		const newestPersonName = `${newestPerson.firstName} ${newestPerson.lastName}`;
+		document.title = newestPersonName;
+		console.log('useEffect');
+		return () => {
+			console.log('Unmounted');
+		}
+	}, [newestPerson]);
 
-    return (
-        <div className='col col-sm-12'>
-            <h2 className='mt-4 text-center'>
-                Newest Person: {`${newestPerson.firstName} ${newestPerson.lastName}`}
-            </h2>
-            <PeopleCount />
-        </div>
-    );
+	return (
+		<div className='col col-sm-12'>
+			<h2 className='mt-4 text-center'>
+				Newest Person: {`${newestPerson.firstName} ${newestPerson.lastName}`}
+			</h2>
+			<PeopleCount />
+		</div>
+	);
 };
 
 export default NewestPerson;
